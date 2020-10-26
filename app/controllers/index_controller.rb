bb7502173cfea9714733de1ceb5425b1573195a3
Nico <arrvac@gmail.com>
2020-10-26 13:42:54 +0100

    goss

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,5 @@
 class IndexController < ApplicationController
   def index
+    @goss = Gossip.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/welcome/:first_name', to: 'landing_page#welcome'
   get '/contact', to: 'contact#contact'
   get '/team', to: 'team#team'
+  
 end
